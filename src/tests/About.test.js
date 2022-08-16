431686e36139f40ab98b37f4f5f79f767b0e04a1
Marcley <marcleyrosa@gmail.com>
2022-08-16 20:30:30 -0300

    Requisito 2 test About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../pages/About';
+
+describe('Teste o componente <About.js />.', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const aboutTitle = screen
+      .getByRole('heading', { name: /About Pokédex/i, level: 2 });
+
+    expect(aboutTitle).toBeInTheDocument();
+  });
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const textFirstP = screen.getByText(/see more details/i);
+
+    expect(textFirstP).toBeInTheDocument();
+
+    const textSecondP = screen.getByText(/encyclopedia containing all Pokémons/i);
+
+    expect(textSecondP).toBeInTheDocument();
+  });
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const imageAbout = screen.getByRole('img', { name: /pokédex/i });
+    expect(imageAbout).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
