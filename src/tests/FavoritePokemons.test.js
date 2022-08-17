a1e31b90db1aba7f0d46ef4910c09adebcedbedf
Marcley <marcleyrosa@gmail.com>
2022-08-17 08:01:23 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Test se é exibida na tela a mensagem No favorite pokemon found.', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFound = screen.getByText('No favorite pokemon found');
+
+    expect(notFound).toBeInTheDocument();
+  });
+  it('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+});
